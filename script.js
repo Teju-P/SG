7aa9aa6a2e9b410a22f3a873b798ed2e4c5c743c
TEJU P <115681017+Teju-P@users.noreply.github.com>
2025-08-25 09:52:43 +0530

    Changed nav bar for mobile view

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() {
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
 
-    // Smooth scrolling for navigation links
-    const navLinks = document.querySelectorAll('.nav-link');
-
-    navLinks.forEach(link => {
-        link.addEventListener('click', function(e) {
-            // The default action is to jump to the anchor. We prevent that.
-            e.preventDefault();
-            document.querySelector(this.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
-        });
-    });
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
 });
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}));
